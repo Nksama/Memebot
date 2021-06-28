ac75b0cf81d1229b30ad43ec0e1b20999b0f951d
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-06-28 09:31:56 +0530

    Add files via upload

---
+++ meme.py
@@ -0,0 +1,48 @@
+import requests
+from pyrogram import filters , Client 
+import os
+
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
+token = os.environ.get('TOKEN')
+
+
+bot = Client(
+	"memebot"
+	api_id=api_id,
+	api_hash=api_hash,
+	bot_token=token
+	)
+
+
+
+@bot.on_message(filters.command('start'))
+def start(_,message):
+	message.reply_text(start , parse_mode='markdown')
+
+
+
+
+
+@bot.on_message(filters.command('meme'))
+def meme(_,message):
+	r = requests.get('https://nksamamemeapi.pythonanywhere.com').json()
+	pic = r['image']
+	title = r['title']
+	bot.send_photo(message.chat.id , pic , caption=title)
+
+
+
+
+@bot.on_message(filters.command('hmeme') & filters.private)
+def hmeme(_,message):
+	r = requests.get('https://nksamamemeapi.pythonanywhere.com/get/hentaimemes').json()
+	pic = r['image']
+	title = r['title']
+	bot.send_photo(message.chat.id , pic , caption=title)
+
+
+
+
+
+bot.run()
